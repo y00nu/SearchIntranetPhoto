ec70bf4c6cf12b9814f436d735a58cea3bc8a141
y00nu <zaq12wsx41@gmail.com>
2022-11-12 01:11:06 +0900

    Favicon Update

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 let input = document.querySelector('#num');
 input.addEventListener('keydown', e=>{
     if(e.keyCode === 13){
-        window.open('https://www.google.com');
+        window.open('https://www.google.com',);
     }
 })
